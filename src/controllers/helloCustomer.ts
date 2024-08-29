c77c0f89ab843444016a8aedad9b214cbf8f190c
ohmmaagoch <phatcharaphol@hotmail.com>
2024-08-30 01:02:56 +0700

    reset

--- src/controllers/helloCustomer.ts
+++
@@ -1,5 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-
-export async function helloCustomer(req: Request, res: Response, next: NextFunction) {
-    res.send('Hello from customers');
-}
